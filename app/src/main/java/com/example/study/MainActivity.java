24a1a019880a208dad131e8209c6b7c569a317a3
gunoo-p <gunwoo405@naver.com>
2024-05-01 17:39:15 +0900

    계산기2

--- app/src/main/java/com/example/study/MainActivity.java
+++ app/src/main/java/com/example/study/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.study;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,37 +31,71 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
     public void buttonClick(View view) {
-        if (view.getId() == R.id.all_clear_btn) {
-            inFirstInput = true;
-            resultNumber = 0;
-            operator = '+';
-            resultText.setTextColor(0xFF000000);
-            resultText.setText(String.valueOf(resultNumber));
-        }
-
 
         Button getButton = findViewById(view.getId());
 
-
-        if (view.getId() >= R.id.num_0_btn && view.getId() <= R.id.num_9_btn) {
-            if (inFirstInput) {
+        switch (view.getId()) {
+            case R.id.all_clear_btn:
+                inFirstInput = true;
+                resultNumber = 0;
+                operator = '+';
                 resultText.setTextColor(0xFF000000);
-                resultText.setText(getButton.getText().toString());
-                inFirstInput = false;
-            } else {
-                resultText.append(getButton.getText().toString());
-            }
+                resultText.setText(String.valueOf(resultNumber));
+                break;
 
-        } else if (view.getId() == R.id.num_2_btn) {
-            if (inFirstInput) {
-                resultText.setTextColor(0xFF000000);
-                resultText.setText(getButton.getText().toString());
-                inFirstInput = false;
-            } else {
-                resultText.append(getButton.getText().toString());
-            }
+            case R.id.num_0_btn:
+            case R.id.num_1_btn:
+            case R.id.num_2_btn:
+            case R.id.num_3_btn:
+            case R.id.num_4_btn:
+            case R.id.num_5_btn:
+            case R.id.num_6_btn:
+            case R.id.num_7_btn:
+            case R.id.num_8_btn:
+            case R.id.num_9_btn:
+                if (inFirstInput) {
+                    resultText.setTextColor(0xFF000000);
+                    resultText.setText(getButton.getText().toString());
+                    inFirstInput = false;
+                } else {
+                    resultText.append(getButton.getText().toString());
+                }
+                break;
+
+            default:
+                Toast.makeText(getApplicationContext(),getButton.getText().toString() + "버튼이 클릭되었습니다.", Toast.LENGTH_LONG).show();
+                break;
         }
+
+//        if (view.getId() == R.id.all_clear_btn) {
+//            inFirstInput = true;
+//            resultNumber = 0;
+//            operator = '+';
+//            resultText.setTextColor(0xFF000000);
+//            resultText.setText(String.valueOf(resultNumber));
+//        }
+//
+//
+//        if (view.getId() == R.id.num_0_btn
+//                || view.getId() == R.id.num_1_btn
+//                || view.getId() == R.id.num_2_btn
+//                || view.getId() == R.id.num_3_btn
+//                || view.getId() == R.id.num_4_btn
+//                || view.getId() == R.id.num_5_btn
+//                || view.getId() == R.id.num_6_btn
+//                || view.getId() == R.id.num_7_btn
+//                || view.getId() == R.id.num_8_btn
+//                || view.getId() == R.id.num_9_btn) {
+//
+//            if (inFirstInput) {
+//                resultText.setTextColor(0xFF000000);
+//                resultText.setText(getButton.getText().toString());
+//                inFirstInput = false;
+//            } else {
+//                resultText.append(getButton.getText().toString());
+//            }
+//        }
+
     }
 }
